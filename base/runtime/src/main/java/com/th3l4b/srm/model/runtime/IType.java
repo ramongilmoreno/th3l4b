75511d6bcf668891623ffb7cd1c0dd35da859ae6
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2015-01-31 13:11:59 +0100

    Deprecate types until better support can be built.

--- base/runtime/src/main/java/com/th3l4b/srm/model/runtime/IType.java
+++ base/runtime/src/main/java/com/th3l4b/srm/model/runtime/IType.java
@@ -3,8 +3,11 @@ package com.th3l4b.srm.model.runtime;
 import com.th3l4b.common.data.named.INamed;
 import com.th3l4b.common.data.propertied.IPropertied;
 
+/**
+ * @deprecated Util some types support is built. Meanwhile only string fields.
+ */
 public interface IType<T> extends INamed, IPropertied {
-	T parse (String text) throws Exception;
-	String toString (T value) throws Exception;
-	Class<T> getClazz () throws Exception;
+	T parse(String text) throws Exception;
+	String toString(T value) throws Exception;
+	Class<T> getClazz() throws Exception;
 }
--- base/runtime/src/main/java/com/th3l4b/srm/model/runtime/ITypesContainer.java
+++ base/runtime/src/main/java/com/th3l4b/srm/model/runtime/ITypesContainer.java
@@ -2,5 +2,8 @@ package com.th3l4b.srm.model.runtime;
 
 import com.th3l4b.common.data.named.IContainer;
 
+/**
+ * @deprecated Util some types support is built. Meanwhile only string fields.
+ */
 public interface ITypesContainer extends IContainer<IType<?>> {
 }
