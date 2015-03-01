6f8c65ded8fc1f2ceaf1cd21851b42070894538a
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2015-03-01 03:58:33 +0100

    Minor changes to make mongo finder as the AbstractInMemoryFinder.

--- mongo/src/main/java/com/th3l4b/srm/mongo/AbstractMongoFinder.java
+++ mongo/src/main/java/com/th3l4b/srm/mongo/AbstractMongoFinder.java
@@ -18,8 +18,8 @@ import com.th3l4b.srm.model.runtime.IReverse;
 import com.th3l4b.srm.model.runtime.IReverseRelationship;
 
 public abstract class AbstractMongoFinder implements IFinder {
-	
-	protected abstract IMongoModelRuntime runtime () throws Exception;
+
+	protected abstract IMongoModelRuntime model() throws Exception;
 
 	protected abstract IReverse reverse() throws Exception;
 
@@ -27,7 +27,7 @@ public abstract class AbstractMongoFinder implements IFinder {
 
 	@Override
 	public Collection<IInstance> all(String type) throws Exception {
-		IMongoEntityRuntime mer = runtime().get(type);		
+		IMongoEntityRuntime mer = model().get(type);
 		IEntityRuntime er = mer.runtime();
 		DBCollection collection = getDB().getCollection(mer.collection());
 		BasicDBObject bo = new BasicDBObject(IMongoConstants.FIELD_STATUS,
@@ -46,12 +46,13 @@ public abstract class AbstractMongoFinder implements IFinder {
 			cursor.close();
 		}
 	}
-	
+
 	@Override
 	public IInstance find(IIdentifier id) throws Exception {
-		IMongoEntityRuntime mer = runtime().get(id.getType());
+		IMongoEntityRuntime mer = model().get(id.getType());
 		DBCollection collection = getDB().getCollection(mer.collection());
-		DBObject found = collection.findOne(new BasicDBObject(IMongoConstants.FIELD_ID, id.getKey()));
+		DBObject found = collection.findOne(new BasicDBObject(
+				IMongoConstants.FIELD_ID, id.getKey()));
 		IInstance r = mer.runtime().create();
 		if (found == null) {
 			mer.apply(found, r);
@@ -62,13 +63,14 @@ public abstract class AbstractMongoFinder implements IFinder {
 		}
 		return r;
 	}
-	
+
 	@Override
 	public Collection<IInstance> references(IIdentifier id, String relationship)
 			throws Exception {
 		IReverseRelationship rr = reverse().get(id.getType()).get(relationship);
-		IMongoEntityRuntime mer = runtime().get(rr.getSourceType());
-		BasicDBObject of = new BasicDBObject(mer.get(rr.getField()).field(), id.getKey());
+		IMongoEntityRuntime mer = model().get(rr.getSourceType());
+		BasicDBObject of = new BasicDBObject(mer.get(rr.getField()).field(),
+				id.getKey());
 		BasicDBObject o = new BasicDBObject(IMongoConstants.FIELD_FIELDS, of);
 		DBCursor find = getDB().getCollection(mer.collection()).find(o);
 		IEntityRuntime er = mer.runtime();
--- mongo/src/main/java/com/th3l4b/srm/mongo/IMongoModelRuntime.java
+++ mongo/src/main/java/com/th3l4b/srm/mongo/IMongoModelRuntime.java
@@ -4,5 +4,5 @@ import com.th3l4b.common.data.named.IContainer;
 import com.th3l4b.srm.model.runtime.IModelRuntime;
 
 public interface IMongoModelRuntime extends IContainer<IMongoEntityRuntime> {
-	IModelRuntime runtime () throws Exception;
+	IModelRuntime runtime() throws Exception;
 }
