2d959f4c5642e4d8759c0c99cf9234a3a9c5f47b
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2015-03-22 20:29:04 +0100

    More complex test. Includes new compare*(a, b) methods for entities in the *ModelUtils

--- sample/base/src/test/java/com/th3l4b/srm/codegen/sample/junit/SampleJsonTest.java
+++ sample/base/src/test/java/com/th3l4b/srm/codegen/sample/junit/SampleJsonTest.java
@@ -1,18 +1,24 @@
 package com.th3l4b.srm.codegen.sample.junit;
 
 import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.th3l4b.srm.json.Generator;
 import com.th3l4b.srm.json.IJsonConstants;
 import com.th3l4b.srm.json.IJsonModelRuntime;
 import com.th3l4b.srm.json.JsonUtils;
 import com.th3l4b.srm.json.Parser;
+import com.th3l4b.srm.json.Parser.Result;
 import com.th3l4b.srm.model.runtime.EntityStatus;
 import com.th3l4b.srm.model.runtime.IInstance;
 import com.th3l4b.srm.sample.base.generated.SampleModelUtils;
 import com.th3l4b.srm.sample.base.generated.entities.IJSONTest;
+import com.th3l4b.srm.sample.base.generated.entities.impl.DefaultJSONTest;
 
 public class SampleJsonTest implements IJsonConstants {
 
@@ -37,6 +43,56 @@ public class SampleJsonTest implements IJsonConstants {
 		Assert.assertEquals(EntityStatus.ToMerge, jt.coordinates().getStatus());
 		Assert.assertEquals("a", jt.getField1());
 		Assert.assertEquals("b", jt.getField2());
+	}
+
+	@Test
+	public void arrayTest() throws Exception {
+		ArrayList<IInstance> r = new ArrayList<IInstance>();
+		String f1 = "Some value of field 1";
+		String f2 = "Some value of field 2";
+		{
+			DefaultJSONTest jt = new DefaultJSONTest();
+			jt.coordinates().setStatus(EntityStatus.ToMerge);
+			jt.setField1(f1);
+			r.add(jt);
+		}
+		{
+			DefaultJSONTest jt = new DefaultJSONTest();
+			jt.coordinates().setStatus(EntityStatus.ToDelete);
+			jt.setField2(f2);
+			r.add(jt);
+		}
+		{
+			DefaultJSONTest jt = new DefaultJSONTest();
+			jt.coordinates().setStatus(EntityStatus.ToSave);
+			jt.setField1(f1);
+			jt.setField2(f2);
+			r.add(jt);
+		}
+		{
+			DefaultJSONTest jt = new DefaultJSONTest();
+			jt.coordinates().setStatus(EntityStatus.Unknown);
+			r.add(jt);
+		}
+		IJsonModelRuntime jmr = JsonUtils.runtime(SampleModelUtils.RUNTIME);
+		StringWriter out = new StringWriter();
+		Generator generator = new Generator(jmr, out);
+		generator.write(r);
+		generator.close();
+
+		StringReader input = new StringReader(out.getBuffer().toString());
+		Parser parser = new Parser(jmr, input);
+		Result result = parser.parse(false, true);
+		Collection<IInstance> many = result._many;
+		Assert.assertNotNull(many);
+		Assert.assertEquals(r.size(), many.size());
+
+		int i = 0;
+		for (IInstance j : many) {
+			IJSONTest a = (IJSONTest) j;
+			IJSONTest b = (IJSONTest) r.get(i++);
+			Assert.assertTrue(SampleModelUtils.compareJSONTest(a, b));
+		}
 	}
 
 }
