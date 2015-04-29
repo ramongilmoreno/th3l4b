57cbaaa3e2331f6c616d762a2068efcf535290e1
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2015-04-29 19:57:17 +0200

    Added transaction support to updater.

--- codegen/java/runtime/src/main/java/com/th3l4b/srm/codegen/java/runtime/AbstractUpdater.java
+++ codegen/java/runtime/src/main/java/com/th3l4b/srm/codegen/java/runtime/AbstractUpdater.java
@@ -16,67 +16,114 @@ public abstract class AbstractUpdater implements IUpdater {
 
 	protected abstract IFinder finder() throws Exception;
 
+	/**
+	 * Called at the first change ({@link #update(IInstance, IInstance)} or
+	 * {@link #insert(IInstance)}) triggered in an {@link #update(Collection)}
+	 * call.
+	 * 
+	 * Empty (no transaction control) as per this base implementation.
+	 * 
+	 * After this method is called, either {@link #endTransaction()} or
+	 * {@link #rollbackTransaction()} will be called.
+	 */
+	protected void beginTransaction() throws Exception {
+	}
+
+	/**
+	 * Called at the end of changes ({@link #update(IInstance, IInstance)} or
+	 * {@link #insert(IInstance)}) in an {@link #update(Collection)} call. Only
+	 * called if any change was made.
+	 * 
+	 * Empty (no transaction control) as per this base implementation.
+	 */
+	protected void endTransaction() throws Exception {
+	}
+
+	/**
+	 * Called at the end of changes ({@link #update(IInstance, IInstance)} or
+	 * {@link #insert(IInstance)}) in an {@link #update(Collection)} call. Only
+	 * called if any change was made and some error happened at commit.
+	 * 
+	 * Empty (no transaction control) as per this base implementation.
+	 */
+	protected void rollbackTransaction() throws Exception {
+	}
+
 	@Override
 	public Collection<IInstance> update(Collection<IInstance> entities)
 			throws Exception {
-		ArrayList<IInstance> r = new ArrayList<IInstance>();
-		for (IInstance e : entities) {
-			if (!e.coordinates().getStatus().isAction()) {
-				continue;
-			}
-			IInstance found = finder().find(e.coordinates().getIdentifier());
-			if (found.coordinates().getStatus() == EntityStatus.Unknown) {
-				found = null;
-			}
-
-			// Decide result
-			EntityStatus newStatus = null;
-			EntityStatus action = e.coordinates().getStatus();
-			switch (action) {
-			case ToSave:
-				newStatus = EntityStatus.Saved;
-				break;
-			case ToDelete:
-				newStatus = EntityStatus.Deleted;
-				break;
-			case ToMerge:
-				if ((found != null)
-						&& (found.coordinates().getStatus() == EntityStatus.Deleted)) {
-					newStatus = EntityStatus.Deleted;
-				} else {
-					newStatus = EntityStatus.Saved;
+		boolean inTransaction = false;
+		boolean transactionEnded = false;
+		try {
+			ArrayList<IInstance> r = new ArrayList<IInstance>();
+			for (IInstance e : entities) {
+				if (!e.coordinates().getStatus().isAction()) {
+					continue;
 				}
-				break;
-			default:
-				throw new IllegalStateException(
-						"Do not know how to handle status: " + action);
+				IInstance found = finder()
+						.find(e.coordinates().getIdentifier());
+				if (found.coordinates().getStatus() == EntityStatus.Unknown) {
+					found = null;
+				}
+
+				// Decide result
+				EntityStatus newStatus = null;
+				EntityStatus action = e.coordinates().getStatus();
+				switch (action) {
+				case ToSave:
+					newStatus = EntityStatus.Saved;
+					break;
+				case ToDelete:
+					newStatus = EntityStatus.Deleted;
+					break;
+				case ToMerge:
+					if ((found != null)
+							&& (found.coordinates().getStatus() == EntityStatus.Deleted)) {
+						newStatus = EntityStatus.Deleted;
+					} else {
+						newStatus = EntityStatus.Saved;
+					}
+					break;
+				default:
+					throw new IllegalStateException(
+							"Do not know how to handle status: " + action);
+				}
+
+				// Create a copy and apply changes
+				IEntityRuntime er = model().get(
+						e.coordinates().getIdentifier().getType());
+				IInstance copy = er.create();
+				if (found != null) {
+					er.copy(found, copy);
+				}
+				er.apply(e, copy);
+				er.unSetNulls(copy);
+				copy.coordinates().setStatus(newStatus);
+
+				// Prepare to return a copy of the copy
+				IInstance copy2 = er.create();
+				er.copy(copy, copy2);
+
+				inTransaction = true;
+				if (found == null) {
+					insert(copy);
+				} else {
+					update(copy, found);
+				}
+
+				r.add(copy2);
 			}
 
-			// Create a copy and apply changes
-			IEntityRuntime er = model().get(
-					e.coordinates().getIdentifier().getType());
-			IInstance copy = er.create();
-			if (found != null) {
-				er.copy(found, copy);
+			if (inTransaction) {
+				endTransaction();
+				transactionEnded = true;
 			}
-			er.apply(e, copy);
-			er.unSetNulls(copy);
-			copy.coordinates().setStatus(newStatus);
-
-			// Prepare to return a copy of the copy
-			IInstance copy2 = er.create();
-			er.copy(copy, copy2);
-
-			if (found == null) {
-				insert(copy);
-			} else {
-				update(copy, found);
+			return r;
+		} finally {
+			if (inTransaction && !transactionEnded) {
+				rollbackTransaction();
 			}
-
-			r.add(copy2);
 		}
-
-		return r;
 	}
 
 	protected abstract void insert(IInstance entity) throws Exception;
